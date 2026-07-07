240040b3ac80341f15e3e80805b13dfa03422ecc
Midnight Builder <builder@midnight.app>
2026-07-07 11:19:49 +0300

    Fix missing TextView import in MediaAdaptor

--- app/src/main/java/ani/dantotsu/media/MediaAdaptor.kt
+++ app/src/main/java/ani/dantotsu/media/MediaAdaptor.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.app.ActivityOptionsCompat
 import androidx.core.content.ContextCompat
